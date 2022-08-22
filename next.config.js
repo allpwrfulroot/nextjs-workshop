edb218a91e7674867245cc3b83701d2a70bfe258
A K <2465035+allpwrfulroot@users.noreply.github.com>
2022-08-22 10:20:30 -0400

    working workshop app

--- next.config.js
+++ next.config.js
@@ -4,21 +4,10 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-  generateStatsFile: true,
-})
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 }
 
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [withMDX, withBundleAnalyzer]
-  return plugins.reduce((acc, plugin) => plugin(acc), {
-    ...defaultConfig,
-    ...nextConfig,
-  })
-}
+module.exports = withMDX(nextConfig)
